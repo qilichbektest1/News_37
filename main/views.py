9a231b0ee406261f214044fc16f6cfa596cba457
Пользователь <mansurovqilichbek39@gmail.com>
2025-12-17 17:06:57 +0500

    O'zgarishlar kiritildi

--- main/views.py
+++ main/views.py
@@ -8,8 +8,10 @@ class HomeView(View):
     def get(self,request):
 
         main_articles = Article.objects.order_by('-important', '-views')[:20]
+        latest_articles = Article.objects.order_by('-created_at')[:10]
 
         context = {
             'main_articles': main_articles,
+            'latest_articles': latest_articles,
         }
         return render(request,'index.html', context)
